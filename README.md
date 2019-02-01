28ed9b257c1275f5503e4d989455d3780d91a847
Eugene <supernovaprotocol@gmail.com>
2019-02-01 16:00:41 +0700

    Add license files, file for 3rd party licenses

---
+++ README.md
@@ -0,0 +1,6 @@
+# License
+This software is licensed under GPL v3 license (see LICENSE).
+
+## Third party libraries licenses
+The software uses third party libraries that are distributed under 
+their own terms (see LICENSE-3RD-PARTY).
--- image_net/batch_download.py
+++ image_net/batch_download.py
@@ -1,5 +1,22 @@
-import os
+# <pyqt-robot - a small Selenium-like API for testing GUI apps written with PyQt.>
+# Copyright © 2019 Evgenii Dolotov. Contacts <supernovaprotocol@gmail.com>
+# Author: Evgenii Dolotov
+# License: https://www.gnu.org/licenses/gpl-3.0.txt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
 from image_net.downloader import get_factory
 from image_net.util import Url2FileName
 
